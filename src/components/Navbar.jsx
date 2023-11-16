a2e44c179533c94d2f52fc1b6fad89e33b329931
Nessaj44 <johnedmoncoc@gmail.com>
2023-11-16 14:30:19 +0800

    Add navbar and font

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export const Navbar = () => {
+  return (
+    <nav className="bg-red-400">
+      <h1 className="font-black text-4xl tracking-normal text-gray-800 p-5">Mangainfinity</h1>
+    </nav>
+  )
+}
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,11 @@
 import { MangaList } from "../components/MangaList"
+import { Navbar } from "../components/Navbar"
 
 
 export const Home = () => {
   return (
     <>
+      <Navbar />
       <MangaList />
     </>
   )
