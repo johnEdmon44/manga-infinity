2cf1cbc6b30a10efc25a9d91d6847ba6feb54835
Nessaj44 <johnedmoncoc@gmail.com>
2023-11-19 19:04:20 +0800

    Update navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,7 +1,12 @@
 export const Navbar = () => {
   return (
-    <nav className="bg-red-400">
-      <h1 className="font-black text-4xl tracking-normal text-gray-800 p-5">Mangainfinity</h1>
+    <nav className="">
+      <h1 className="font-black text-4xl tracking-normal text-gray-800 p-5 bg-white">Mangainfinity</h1>
+
+      <ul className="bg-red-500 mb-10 text-white font-bold flex gap-10 p-5">
+        <li className="cursor-pointer">Home</li>
+        <li className="cursor-pointer">Bookmark</li>
+      </ul>
     </nav>
   )
 }
