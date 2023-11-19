4a318b25cce9b4bbbe5de4905e6fddfc43c49f96
Nessaj44 <johnedmoncoc@gmail.com>
2023-11-19 16:12:02 +0800

    Add hover effect

--- src/components/MangaList.jsx
+++ src/components/MangaList.jsx
@@ -8,7 +8,6 @@ export const MangaList = ({ genre, order, limit }) => {
     return <h1>Loading...</h1>
   }
 
-  console.log(mangaList.data)
 
   return (
     <section className="bg-white mx-auto w-3/4 mt-10 rounded-lg">
@@ -16,9 +15,18 @@ export const MangaList = ({ genre, order, limit }) => {
 
       <div className="flex justify-center gap-8 p-5">
         {mangaList.data.map(manga => (
-          <div key={manga.mal_id} className="relative cursor-pointer">
-            <img className="h-80 object-cover border border-gray-300" src={manga.images.webp.image_url}></img>
-            <h1 className="bg-black bg-opacity-70 absolute w-full bottom-0 text-center text-white">{manga.title}</h1>
+          <div key={manga.mal_id} className=" relative group">
+            <img className="h-80 w-80 object-cover border border-gray-300 group-hover:opacity-30 transition-all" src={manga.images.webp.image_url}></img>
+            <div className="absolute top-0  mt-5 w-full invisible text-center group-hover:visible">
+              {manga.genres.map(genre => (
+                <p className="bg-black m-2 p-1 bg-opacity-50 text-white inline-flex cursor-pointer transition-all hover:bg-opacity-100 rounded-md" 
+                key={genre.mal_id}>{genre.name}
+                </p>
+              ))}
+            </div>
+            <h1 className="bg-black bg-opacity-70 absolute w-full bottom-0 text-center text-white cursor-pointer hover:bg-opacity-100">
+              {manga.title}
+            </h1>
           </div>
         ))}
       </div>
