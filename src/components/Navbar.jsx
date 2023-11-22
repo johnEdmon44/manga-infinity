2f116ea0e080c3995b6cb921d8108355a29aefc9
Nessaj44 <johnedmoncoc@gmail.com>
2023-11-22 13:49:18 +0800

    Update browse link in navbar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -9,7 +9,7 @@ export const Navbar = () => {
 
       <ul className="bg-red-500 mb-10 text-white font-bold flex gap-10 p-5">
         <Link to={'/'}>Home</Link>
-        <Link to={'/'}>Browse</Link>
+        <Link to={'/browse/1'}>Browse</Link>
         <Link to={'/'}>Bookmark</Link>
       </ul>
     </nav>
---
+++ src/hooks/useFetchGenre.js
@@ -0,0 +1,5 @@
+import { useState } from "react"
+
+export const useFetchGenre = () => {
+  const [genre, setGenre] = useState(null);
+}
