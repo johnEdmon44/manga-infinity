cd586fb029095c8431be14fd41f9c884f84ea660
Nessaj44 <johnedmoncoc@gmail.com>
2024-01-09 11:59:20 +0800

    Add more details

--- src/components/MangaComponent.jsx
+++ src/components/MangaComponent.jsx
@@ -2,6 +2,8 @@ import { useParams } from "react-router-dom"
 import { useFetchManga } from "../hooks/useFetchManga";
 import { Link } from "react-router-dom";
 import { Button } from "./Button";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faEye } from '@fortawesome/free-solid-svg-icons';
 
 export const MangaComponent = () => {
   const { mangaId } = useParams();
@@ -19,6 +21,7 @@ export const MangaComponent = () => {
         <img className="" src={manga.images.webp.image_url} alt={manga.title}></img>
         <div>
           <h1 className="ml-3 mt-1 font-bold text-2xl">{manga.title}</h1>
+
           <div>
             {manga.genres.map(genre => (
               <Link to={`/browse/${genre.mal_id}`} className="bg-black ml-3 mt-1 p-1 bg-opacity-50 text-white inline-flex cursor-pointer transition-all hover:bg-opacity-100 rounded-2xl text-xs" id={genre.mal_id} key={genre.mal_id}>
@@ -26,13 +29,21 @@ export const MangaComponent = () => {
               </Link>
             ))}
           </div>
+
           <div className="flex">
             {manga.authors.map((author, index) => (
-              <p className="ml-3 mt-1 text-sm font-bold" id={author.mal_id} key={author.mal_id}>{author.name}{index < manga.authors.length - 1 && <span>, </span>}</p>
+              <p className="ml-3 mt-1 text-sm font-bold" id={author.mal_id} key={author.mal_id}>{author.name}{index < manga.authors.length - 1 && <span> |</span>}</p>
             ))}
           </div>
 
+          <p className="ml-3 mt-3 text-xs text-gray-500">{manga.published.string}</p>
+
           <p className="ml-3 mt-3 text-sm">{manga.synopsis}</p>
+
+          <p className="ml-3 mt-3 text-sm">
+            <FontAwesomeIcon icon={faEye} className="mr-1" />
+            {manga.favorites}
+          </p>
           
           <Button manga={manga} />
         </div>
