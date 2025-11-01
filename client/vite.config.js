516877e13d636c824734c9bfd1e56dbac016ab22
Nessaj44 <johnedmoncoc@gmail.com>
2025-11-01 20:42:25 +0800

    Use env in vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,16 +1,18 @@
-import { defineConfig } from 'vite'
+// vite.config.js
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [react()],
-  server: {
-    proxy: {
-      '/api': {
-        target: 'http://localhost:3000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/api'),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, '.', '')
+  return {
+    plugins: [react()],
+    server: {
+      proxy: {
+        '/api': {
+          target: env.VITE_API_URL,
+          changeOrigin: true,
+        },
       },
-    }
+    },
   }
 })
