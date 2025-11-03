8b302b8d71a3bee1d5f3ea0255ce58a1777864d8
Nessaj44 <johnedmoncoc@gmail.com>
2025-11-03 17:05:31 +0800

    Set up Express server with proxy middleware and move frontend files to client

---
+++ server/server.js
@@ -0,0 +1,22 @@
+require("dotenv").config();
+const express = require("express");
+const { createProxyMiddleware } = require('http-proxy-middleware');
+
+const app = express();
+
+const jikanProxy = createProxyMiddleware({
+  target: 'https://api.jikan.moe/v4',
+  changeOrigin: true,
+});
+
+
+app.get('/', (req, res) => res.send('express'));
+const PORT = 3000;
+
+app.listen(PORT, (err) => {
+  if(err) {
+    throw err;
+  }
+
+  console.log(`server runnign on  ${PORT}`);
+})
